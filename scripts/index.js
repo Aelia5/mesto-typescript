3989d1677214862c0fb25f41d614c5f760409130
Olga Liubimova <lov800@yandex.ru>
2023-06-07 11:30:22 +0300

    Добавить начальные карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+//Профиль
+
 let form = document.querySelector('.popup__form');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
@@ -30,4 +32,44 @@ editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
 form.addEventListener('submit', saveData);
+
+// Начальные карточки
+
+const cardTemplate = document.querySelector('#card').content;
+const cardsList = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Байкал',
+    link: 'https://aelia5.github.io/mesto/images/baikal.jpg'
+  },
+  {
+    name: 'Эльбрус',
+    link: 'https://aelia5.github.io/mesto/images/elbrus.jpg'
+  },
+  {
+    name: 'Кунгурская пещера',
+    link: 'https://aelia5.github.io/mesto/images/kungur.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://aelia5.github.io/mesto/images/dombay.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://aelia5.github.io/mesto/images/kamchatka.jpg'
+  },
+  {
+    name: 'Куршская коса',
+    link: 'https://aelia5.github.io/mesto/images/kosa.jpg'
+  }
+];
+
+initialCards.forEach(function (item) {
+  let cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__image').src = item.link;
+  cardElement.querySelector('.element__title').textContent = item.name;
+  cardsList.append(cardElement);
+}
+)
 
